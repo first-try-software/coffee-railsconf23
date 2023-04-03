first-try-software/coffee

STORY: As a patron, I want hot apple cider
Co-authored-by: Fito von Zastrow <adolfovon@gmail.com>

spec/beverage_factory_spec.rb
@@ -23,6 +23,10 @@ RSpec.describe BeverageFactory do
       expect(described_class.build(:soup, driver, options))
         .to be_an_instance_of(Soup)
         .and(have_attributes(driver: driver, options: options))
+
+      expect(described_class.build(:cider, driver, options))
+        .to be_an_instance_of(Cider)
+        .and(have_attributes(driver: driver, options: options))
     end
 
     it "returns a null object when the requested beverage is not supported" do

first-try-software/coffee-railsconf23

spec/beverage_factory_spec.rb
@@ -23,6 +23,10 @@ RSpec.describe BeverageFactory do
       expect(described_class.build(:tomato_soup, driver, options))
         .to be_an_instance_of(TomatoSoup)
         .and(have_attributes(driver: driver, options: options))
+
+      expect(described_class.build(:apple_cider, driver, options))
+        .to be_an_instance_of(AppleCider)
+        .and(have_attributes(driver: driver, options: options))
     end
 
     it "returns a null object when the requested beverage is not supported" do